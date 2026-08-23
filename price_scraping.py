2aa107dcbb0ab3470f2b288e8245fb0c65455ca4
k3vina <kevinajepkonga@gmail.com>
2026-08-23 15:19:46 +0300

    store the scraped items in a list, save in a CSV file, and concert the prices from KES to USD

--- price_scraping.py
+++ price_scraping.py
@@ -1,7 +1,6 @@
 import requests
 from requests.exceptions import HTTPError, Timeout
 from bs4 import BeautifulSoup
-import pandas as pd
 
 try:
 
@@ -31,6 +30,28 @@ try:
     items = soup.select(".itm.col")
     #print(items)
 
+    all_scraped_items = []                  
+
+    for item in items[:10]:                      # limit to first 10
+        title = item.select_one(".itm col").text
+        price_text = item.select_one(".prc").text   # e.g. "KSh 1,080"
+
+        price_kes = float(price_text.replace("KSh", "").replace(",", "").split("-")[0].strip())
+        price_usd = price_kes / usd_rate
+
+        item_dict = {
+            "product_title": title,
+            "price_kes": price_kes,
+            "price_gbp": round(price_gbp, 2),
+        }
+
+        all_scraped_items.append(item_dict)
+
+    #print(all_scraped_items)
+
+    df = pd.DataFrame(all_scraped_items)
+    #print(df)
+    df.to_csv("extracted_items.csv")
 
 except Timeout:
     print("The server took long to respond") # prints 429 code
